7c09a60feeb5641d63d22ebcbe0839458f6992c8
danielk43 <11856117+danielk43@users.noreply.github.com>
2021-12-13 19:14:34 -0500

    Modify apk install list

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -12,14 +12,8 @@ PRODUCT_PACKAGES += \
     Eleven \
     Etar \
     ExactCalculator \
-    Jelly \
     Profiles \
     Seedvault
-
-ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
-PRODUCT_PACKAGES += \
-    AudioFX
-endif
 
 ifeq ($(PRODUCT_TYPE), go)
 PRODUCT_PACKAGES += \
@@ -62,3 +56,19 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # SystemUI plugins
 PRODUCT_PACKAGES += \
     QuickAccessWallet
+
+# Include Chromium (Bromite) web browser
+PRODUCT_PACKAGES += \
+    Chromium
+
+# Include Partner GMS Vendor additions
+PRODUCT_PACKAGES += \
+    GmsCore \
+    GsfProxy \
+    FakeStore \
+    LocalGsmNlpBackend \
+    NominatimGeocoderBackend \
+    PdfViewer \
+    AuroraServices \
+    AuroraStore \
+    AuroraDroid
